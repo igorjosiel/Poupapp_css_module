4ab65f46ed1a05d2ed3c2a75c8e89d825f748d23
Igor Josiel <igorjosielvitalino@gmail.com>
2025-12-01 14:46:07 -0300

    Iniciando a aplicação.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <>
+      <aside>
+        <img src={logo} className="App-logo" alt="logo" />
+      </aside>
+      <h1>Poupapp</h1>
+    </>
+  )
+}
+
+export default App
