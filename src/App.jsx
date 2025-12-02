a4e5a608210e3965aee4b2c4649546689df7244b
Igor Josiel <igorjosielvitalino@gmail.com>
2025-12-02 17:09:29 -0300

    Cria o componente Card e seus elementos internos: Header e Body.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Aside from "./components/Aside";
 import SearchInput from "./components/SearchInput";
 import Main from "./components/Main";
 import Typography from "./components/Typography";
+import Card from "./components/Card";
 
 function App() {
   return (
@@ -20,6 +21,44 @@ function App() {
             Veja como estão suas finanças hoje.
           </Typography>
         </div>
+
+        <section>
+          <Card>
+            <Card.Header>
+              Orçamento diário disponível
+            </Card.Header>
+            <Card.Body>
+              R$ 200,00
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header>
+              Progresso da meta financeira
+            </Card.Header>
+            <Card.Body>
+              R$ 200,00
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header>
+              Movimentação financeira
+            </Card.Header>
+            <Card.Body>
+              R$ 200,00
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header>
+              Minhas contas
+            </Card.Header>
+            <Card.Body>
+              R$ 200,00
+            </Card.Body>
+          </Card>
+        </section>
       </Main>
     </Container>
   );
