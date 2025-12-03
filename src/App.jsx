7166d3381548a848d0a1b82a49a2ff3e19a16a20
Igor Josiel <igorjosielvitalino@gmail.com>
2025-12-02 23:04:08 -0300

    fix: ajusta o layout e posicionamento dos cards.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Typography from "./components/Typography";
 import Card from "./components/Card";
 import DailyBudget from "./components/DailyBudget";
 import SavingsStatus from "./components/SavingsStatus";
+import styles from "./app.module.css";
 
 function App() {
   return (
@@ -13,54 +14,42 @@ function App() {
       <Aside />
 
       <Main>
-        <SearchInput />
+        <div className={styles.container}>
+          <SearchInput />
 
-        <div>
-          <Typography variant="h1">
-            Olá, Igor!
-          </Typography>
-          <Typography variant="body">
-            Veja como estão suas finanças hoje.
-          </Typography>
+          <div>
+            <Typography variant="h1">Olá, Igor!</Typography>
+            <Typography variant="body">
+              Veja como estão suas finanças hoje.
+            </Typography>
+          </div>
+
+          <section className={styles.grid}>
+            <Card>
+              <Card.Header>Orçamento diário disponível</Card.Header>
+              <Card.Body>
+                <DailyBudget value={250} />
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>Progresso da meta financeira</Card.Header>
+              <Card.Body>
+                <SavingsStatus percent={25} />
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>Movimentação financeira</Card.Header>
+              <Card.Body>R$ 200,00</Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header>Minhas contas</Card.Header>
+              <Card.Body>R$ 200,00</Card.Body>
+            </Card>
+          </section>
         </div>
-
-        <section>
-          <Card>
-            <Card.Header>
-              Orçamento diário disponível
-            </Card.Header>
-            <Card.Body>
-              <DailyBudget value={250} />
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card.Header>
-              Progresso da meta financeira
-            </Card.Header>
-            <Card.Body>
-              <SavingsStatus percent={25} />
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card.Header>
-              Movimentação financeira
-            </Card.Header>
-            <Card.Body>
-              R$ 200,00
-            </Card.Body>
-          </Card>
-
-          <Card>
-            <Card.Header>
-              Minhas contas
-            </Card.Header>
-            <Card.Body>
-              R$ 200,00
-            </Card.Body>
-          </Card>
-        </section>
       </Main>
     </Container>
   );
